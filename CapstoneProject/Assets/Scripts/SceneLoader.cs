c0be4cc8d958c5c211cd5052be24b2a5099273b4
Shaina Muli <smuli@my.centennialcollege.ca>
2021-09-15 17:33:31 -0400

    Added MainMenuScene and MainGameScene, created simple UI layout. Added SceneLoader script.

---
+++ CapstoneProject/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void SwitchToScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Game has quit :3");
+        Application.Quit();
+    }
+
+}
