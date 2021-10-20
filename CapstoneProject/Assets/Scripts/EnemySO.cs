b7c690ecc2a81ee8f03f9ffd82b79adaf4465ad2
Merik <m.ally94@yahoo.ca>
2021-10-20 14:35:44 -0400

    Added TargetSleect Icon Animation

--- CapstoneProject/Assets/Scripts/EnemySO.cs
+++ CapstoneProject/Assets/Scripts/EnemySO.cs
@@ -17,5 +17,5 @@ public class EnemySO : ScriptableObject
     public AnimationReferenceAsset attack;
     public AnimationReferenceAsset damage;
     
-    public AudioClip sfx;
+    public AudioClip attackSFX;
 }
