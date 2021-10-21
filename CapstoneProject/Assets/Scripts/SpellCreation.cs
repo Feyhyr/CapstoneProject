8828085cd4f507c32be47faf66c875f3c1725f6b
Imawaffle <cchang61@my.centennialcollege.ca>
2021-10-21 11:55:59 -0400

    Added audio effects into spell creations

--- CapstoneProject/Assets/Scripts/SpellCreation.cs
+++ CapstoneProject/Assets/Scripts/SpellCreation.cs
@@ -7,10 +7,13 @@ public class SpellCreation : MonoBehaviour
     public SpellSO spell;
     public string spellName;
     public int damage;
+    public AudioClip sfx;
 
     private void Start()
     {
         damage = spell.sDamage;
+        sfx = spell.sfx;
+        AudioManager.Instance.Play(sfx);
         Destroy(gameObject, 5.0f);
     }
 }
