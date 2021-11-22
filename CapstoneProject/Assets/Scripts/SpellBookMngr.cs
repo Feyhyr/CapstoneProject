a2eb10d422c664b62ac0be027b7bb5ede43f7980
Imawaffle <cchang61@my.centennialcollege.ca>
2021-11-21 21:21:55 -0500

    Update SpellBookMngr.cs

--- CapstoneProject/Assets/Scripts/SpellBookMngr.cs
+++ CapstoneProject/Assets/Scripts/SpellBookMngr.cs
@@ -18,7 +18,7 @@ public class SpellBookMngr : Singleton<SpellBookMngr>
     public GameObject textbox;
     public Transform container;
     public bool canOpen;
-    [TextArea(15, 30)]
+    [TextArea(15, 100)]
     public List<string> spellInfoList;
 
     private new void Awake()
